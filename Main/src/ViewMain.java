c5e115074c5c234756ab6c85ef2dce3e439d3d1f
tomturner1998 <twt203@exeter.ac.uk>
2019-02-14 12:58:13 +0000

    trying to get the perspective of the fret board

--- Main/src/ViewMain.java
+++ Main/src/ViewMain.java
@@ -1,97 +1,64 @@
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Polygon;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
-public class ViewMain {
-  private JPanel panel;
+public class ViewMain extends JFrame {
+  private BufferedImage background;
+  private MainPanel panel;
+  private int img_height;
+  private int img_width;
 
   public ViewMain() {
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.add(new AnimatePanel());
-    frame.pack();
-    frame.setSize(500,1000);
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
+    panel = new MainPanel();
+    this.add(panel);
+    this.pack();
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.setVisible(true);
   }
 
-  private class AnimatePanel extends JPanel {
-    private static final int WIDTH = 500;
-    private static final int HEIGHT = 1000;
-    private static final int INC = 10;
-
-    private BufferedImage background;
-
-    private int dx1, dy1, dx2, dy2;
-    private int srcx1, srcy1, srcx2, srcy2;
-    private int img_height;
-
-    public AnimatePanel() {
+  private class MainPanel extends JPanel {
+    public MainPanel() {
       initImages();
-      initImagePoints();
-      Timer timer = new Timer(40, new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          moveBackground();
-          repaint();
-        }
-      });
-      timer.start();
+    }
 
-      FlowLayout layout = (FlowLayout)getLayout();
-      layout.setHgap(0);
-      layout.setVgap(0);
+    @Override
+    public Dimension getPreferredSize() {
+      return new Dimension(img_width, img_height);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
       super.paintComponent(g);
-      g.setColor(Color.WHITE);
-      g.fillRect(0,0,getWidth(), getHeight());
-      g.drawImage(background, dx1, dy1, dx2, dy2, srcx1, srcy1, srcx2, srcy2, this);
-    }
+      Polygon p = new Polygon();
+      p.addPoint(50, 0);
+      p.addPoint(0, img_height);
+      p.addPoint(img_width, img_height);
+      p.addPoint(img_width-50, 0);
+      g.setClip(p);
+      g.drawImage(background, img_width/2-100,0, this);
 
-    public Dimension getPreferedSize() {
-      return new Dimension(500, 1000);
-    }
-
-    private void initImagePoints() {
-      dx1 = 0;
-      dy1 = 0;
-
-      dx2 = WIDTH;
-      dy2 = HEIGHT;
-
-      srcx1 = 0;
-      srcy1 = 0;
-
-      srcx2 = WIDTH;
-      srcy2 = HEIGHT;
+      g.setClip(null);
+      g.setColor(Color.BLUE);
+      g.fillRect(0, img_height/2, img_width,6);
+      int[] arrowXs = {img_width/2-10, img_width/2, img_width/2+10};
+      int[] arrowYs = {img_height/2+2, img_height/2+16, img_height/2+2};
+      g.fillPolygon(arrowXs, arrowYs, 3);
+      g.fillRect(0, img_height/2+100, img_width,6);
     }
 
     private void initImages() {
       try {
         background = ImageIO.read(getClass().getResource("background.png"));
         img_height = background.getHeight();
+        img_width = background.getWidth()+100;
       }catch(Exception e) {
         System.out.println(e.getMessage());
-      }
-    }
-
-    private void moveBackground() {
-      if(srcy1 > img_height) {
-        srcy1 = 0 - HEIGHT;
-        srcy2 = 0;
-      }else {
-        srcy1 += INC;
-        srcy2 += INC;
+        System.exit(1);
       }
     }
   }
