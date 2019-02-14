7069684f9b7beaa71c0672409dd9fd5fe60e12a0
MorganC46 <morgancentini38@gmail.com>
2019-02-14 17:03:07 +0000

    pseudo code for carousel
    This is pretty weak code but it is just a pseudo code for creating a frame for the carousel and adding buttons. The buttons still need to be organised on screen and then functionality added for their movement.

--- Main/src/ViewMain.java
+++ Main/src/ViewMain.java
@@ -22,4 +22,29 @@ public class ViewMain extends JFrame {
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     this.setVisible(true);
   }
+
+  //Pseudo Code for Carousel Menu
+  public CarouselMain() {
+      JFrame carousel = new JFrame("Slash Mode");
+      carousel.setSize((128*5), 128);
+      //carousel.setLocation(0,0); //To be determined
+
+      ImageIcon exit = new ImageIcon("exit.png");
+      ImageIcon select = new ImageIcon("select.png");
+      ImageIcon play = new ImageIcon("play.png");
+      ImageIcon store = new ImageIcon("store.png");
+      ImageIcon tutorial = new ImageIcon("tutorial.png");
+	  
+	  JButton exitButton = new JButton(exit);
+	  JButton selectButton = new JButton(select);
+	  JButton playButton = new JButton(play);
+	  JButton storeButton = new JButton(store);
+	  JButton tutorialButton = new JButton(tutorial);
+	  
+	  carousel.add(exitButton);
+	  carousel.add(selectButton);
+	  carousel.add(playButton);
+	  carousel.add(storeButton);
+	  carousel.add(tutorialButton);
+  }
 }
