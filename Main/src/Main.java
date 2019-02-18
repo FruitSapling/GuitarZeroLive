1cba364a91349209bd1137e0f59d8ea303483c5c
tomturner1998 <twt203@exeter.ac.uk>
2019-02-18 13:51:11 +0000

    Change to main

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,14 @@
-public class Main {
-  public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  public void start(Stage primaryStage) {
     Thread t = new Thread(new GuitarPollingThread());
     t.start();
     ViewMain mainMenu = new ViewMain();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
   }
 }
