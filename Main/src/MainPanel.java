5b0c768b246df9355286d850c61c5e85a2a85cce
MorganC46 <morgancentini38@gmail.com>
2019-02-20 13:49:22 +0000

    Major tidy up of GUI code

--- Main/src/MainPanel.java
+++ Main/src/MainPanel.java
@@ -42,110 +42,36 @@ public class MainPanel extends JPanel {
     g2.fillPolygon(arrow);
   }
 
-  public JButton generateButton(String imageFileName) {
+  public JButton generateButton(String imageFileName, int mode) {
     ImageIcon image = new ImageIcon(imageFileName);
     JButton button = new JButton(image);
+    button.setBackground(Color.WHITE);
+    button.setBorderPainted(false);
+
+    button.addMouseListener( new MouseAdapter() {
+        public void mouseClicked(MouseEvent e) {
+            switch(mode) {
+                case 0: System.exit(0);
+                case 1: System.out.println("");//TODO: Play Mode
+                case 2: System.out.println("");//TODO: Store Mode
+                case 3: System.out.println("");//TODO: Store Mode
+                case 4: System.out.println("");//TODO: Tutorial Mode
+            }
+        }
+    });
     return button;
   }
 
-  public void guitar(Graphics2D g2) {
-    Polygon board = new Polygon();
-    board.addPoint(100, 0);
-    board.addPoint(100, getHeight());
-    board.addPoint(getWidth()-100, getHeight());
-    board.addPoint(getWidth()-100, 0);
-    g2.setClip(board);
-    g2.fillPolygon(board);
-
-    g2.setStroke(new BasicStroke(5));
-    g2.setColor(Color.RED);
-    g2.drawLine(0, 100, getWidth(), 100);
-    g2.drawLine(0, 200, getWidth(), 200);
-    g2.drawLine(0, 300, getWidth(), 300);
-    g2.drawLine(0, 400, getWidth(), 400);
-    g2.drawLine(0, 500, getWidth(), 500);
-    g2.fillRect(0, 600, getWidth(), 50);
-
-    g2.setColor(Color.orange);
-    g2.drawLine(125, 0, 125, getHeight());
-    g2.drawLine(175, 0, 175, getHeight());
-    g2.setStroke(new BasicStroke(4));
-    g2.drawLine(225, 0, 225, getHeight());
-    g2.setStroke(new BasicStroke(3));
-    g2.setColor(Color.GRAY);
-    g2.drawLine(275, 0, 275, getHeight());
-    g2.setStroke(new BasicStroke(2));
-    g2.drawLine(325, 0, 325, getHeight());
-    g2.setStroke(new BasicStroke(2));
-    g2.drawLine(375, 0, 375, getHeight());
-  }
-
-
-
   public void buttons(MainPanel panel){
-    JButton exit = generateButton("Main/src/resources/exit.png");
-    exit.setBackground(Color.WHITE);
-    exit.setBorderPainted(false);
-    exit.addMouseListener( new MouseAdapter() {
-      public void mouseClicked(MouseEvent e) {
-        System.exit(0);
-      }
-      public void mouseEntered(MouseEvent e) {
-        //TODO: Add code for showing which mode this button represents
-      }
-    });
-
-    JButton select = generateButton("Main/src/resources/select.png");
-    select.setBackground(Color.WHITE);
-    select.setBorderPainted(false);
-    select.addMouseListener( new MouseAdapter() {
-      public void mouseClicked(MouseEvent e) {
-        //TODO: Add code for changing GUI to select mode
-      }
-      public void mouseEntered(MouseEvent e) {
-        //TODO: Add code for showing which mode this button represents
-      }
-    });
-
-    JButton play = generateButton("Main/src/resources/play.png");
-    play.setBackground(Color.WHITE);
-    play.setBorderPainted(false);
-    play.addMouseListener( new MouseAdapter() {
-      public void mouseClicked(MouseEvent e) {
-        //TODO: Add code for changing GUI to play mode
-      }
-      public void mouseEntered(MouseEvent e) {
-        //TODO: Add code for showing which mode this button represents
-      }
-    });
-
-    JButton store = generateButton("Main/src/resources/store.png");
-    store.setBackground(Color.WHITE);
-    store.setBorderPainted(false);
-    store.addMouseListener( new MouseAdapter() {
-      public void mouseClicked(MouseEvent e) {
-        //TODO: Add code for changing GUI to store mode
-      }
-      public void mouseEntered(MouseEvent e) {
-        //TODO: Add code for showing which mode this button represents
-      }
-    });
-
-    JButton tutorial = generateButton("Main/src/resources/tutorial.png");
-    tutorial.setBackground(Color.WHITE);
-    tutorial.setBorderPainted(false);
-    tutorial.addMouseListener( new MouseAdapter() {
-      public void mouseClicked(MouseEvent e) {
-        //TODO: Add code for changing GUI to tutorial mode
-      }
-      public void mouseEntered(MouseEvent e) {
-        //TODO: Add code for showing which mode this button represents
-      }
-    });
+    JButton exit = generateButton("Main/src/resources/exit.png", 0);
+    JButton play = generateButton("Main/src/resources/play.png", 1);
+    JButton select = generateButton("Main/src/resources/select.png", 2);
+    JButton store = generateButton("Main/src/resources/store.png", 3);
+    JButton tutorial = generateButton("Main/src/resources/tutorial.png", 4);
 
     exit.setBounds(25, 388, 128, 128); panel.add(exit);
-    select.setBounds(153, 388, 128, 128); panel.add(select);
     play.setBounds(281, 388, 128, 128); panel.add(play);
+    select.setBounds(153, 388, 128, 128); panel.add(select);
     store.setBounds(409, 388, 128, 128); panel.add(store);
     tutorial.setBounds(537, 388, 128, 128); panel.add(tutorial);
 
@@ -158,5 +84,7 @@ public class MainPanel extends JPanel {
     panel.revalidate();
     panel.repaint();
   }
+
+
 
 }
