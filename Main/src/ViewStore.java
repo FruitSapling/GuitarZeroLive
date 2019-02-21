b6af40193ba5688ed0f0de54489678384e7d29a6
MorganC46 <morgancentini38@gmail.com>
2019-02-21 17:33:34 +0000

    Store Manager File Browsers + GUI

--- Main/src/ViewStore.java
+++ Main/src/ViewStore.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 import javax.swing.*;
 
 public class ViewStore extends JFrame {
@@ -13,6 +14,7 @@ public class ViewStore extends JFrame {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.WHITE);
                 g2.fillRect(0, 0, getWidth(), getHeight());
+                menuTitle(storePanel);
                 buttons(storePanel);
             }
         };
@@ -32,16 +34,62 @@ public class ViewStore extends JFrame {
             return new Dimension(400, 250);
         }
 
+        public void menuTitle(StoreManagerPanel panel) {
+            JLabel title = new JLabel("Store Manager Mode");
+            title.setHorizontalAlignment(JLabel.CENTER);
+            title.setVerticalAlignment(JLabel.CENTER);
+            title.setFont(new Font("Arial", Font.PLAIN, 32));
+
+            panel.add(title, BorderLayout.NORTH);
+        }
+
+        public JButton generateFileBrowserButton(StoreManagerPanel panel, int mode) {
+            JButton button = new JButton("Browse...");
+            button.addMouseListener( new MouseAdapter() {
+                public void mouseClicked(MouseEvent e) {
+                    FileZipper.fileBrowser(panel, mode);
+                }
+            });
+            return button;
+        }
         public void buttons(StoreManagerPanel panel){
+
+            JButton titleButton = generateFileBrowserButton(panel, 1);
+            JButton artButton = generateFileBrowserButton(panel, 2);
+            JButton musicButton = generateFileBrowserButton(panel, 3);
+
             JButton saveExit = new JButton("Save");
             saveExit.addMouseListener( new MouseAdapter() {
                 public void mouseClicked(MouseEvent e) {
+                    FileZipper.createZipFile();
                     panel.setVisible(false);
                     dispose();
                 }
             });
-
             saveExit.setPreferredSize(new Dimension(400, 50));
+
+            JPanel eastPanel = new JPanel();
+            GridBagLayout gridBag = new GridBagLayout();
+            GridBagConstraints c = new GridBagConstraints();
+            eastPanel.setLayout(gridBag);
+            eastPanel.setBackground(Color.WHITE);
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.insets = new Insets(10, 0, 10, 0);
+
+            c.gridy = 0;
+            gridBag.setConstraints(titleButton, c);
+            c.gridy = 1;
+            gridBag.setConstraints(artButton, c);
+            c.gridy = 2;
+            gridBag.setConstraints(musicButton, c);
+
+
+            eastPanel.add(titleButton);
+            eastPanel.add(artButton);
+            eastPanel.add(musicButton);
+
+            panel.add(eastPanel, BorderLayout.EAST);
             panel.add(saveExit, BorderLayout.SOUTH);
 
             panel.revalidate();
