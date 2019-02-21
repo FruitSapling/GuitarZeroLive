b198cca015c0adb5d09d8e705506fda57945b1df
tomturner1998 <twt203@exeter.ac.uk>
2019-02-21 10:42:08 +0000

    Changed some indentin bruh

--- Main/src/ViewMain.java
+++ Main/src/ViewMain.java
@@ -1,7 +1,5 @@
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-
 
 public class ViewMain extends JFrame {
   private MainPanel mainPanel;
@@ -31,36 +29,36 @@ public class ViewMain extends JFrame {
     this.setVisible(true);
   }
 
-    public void guitar(Graphics2D g2) {
-        Polygon board = new Polygon();
-        board.addPoint(100, 0);
-        board.addPoint(100, getHeight());
-        board.addPoint(getWidth()-100, getHeight());
-        board.addPoint(getWidth()-100, 0);
-        g2.setClip(board);
-        g2.fillPolygon(board);
+  public void guitar(Graphics2D g2) {
+    Polygon board = new Polygon();
+    board.addPoint(100, 0);
+    board.addPoint(100, getHeight());
+    board.addPoint(getWidth()-100, getHeight());
+    board.addPoint(getWidth()-100, 0);
+    g2.setClip(board);
+    g2.fillPolygon(board);
 
-        g2.setStroke(new BasicStroke(5));
-        g2.setColor(Color.RED);
-        g2.drawLine(0, 100, getWidth(), 100);
-        g2.drawLine(0, 200, getWidth(), 200);
-        g2.drawLine(0, 300, getWidth(), 300);
-        g2.drawLine(0, 400, getWidth(), 400);
-        g2.drawLine(0, 500, getWidth(), 500);
-        g2.fillRect(0, 600, getWidth(), 50);
+    g2.setStroke(new BasicStroke(5));
+    g2.setColor(Color.RED);
+    g2.drawLine(0, 100, getWidth(), 100);
+    g2.drawLine(0, 200, getWidth(), 200);
+    g2.drawLine(0, 300, getWidth(), 300);
+    g2.drawLine(0, 400, getWidth(), 400);
+    g2.drawLine(0, 500, getWidth(), 500);
+    g2.fillRect(0, 600, getWidth(), 50);
 
-        g2.setColor(Color.orange);
-        g2.drawLine(150, 0, 150, getHeight());
-        g2.drawLine(235, 0, 235, getHeight());
-        g2.setStroke(new BasicStroke(4));
-        g2.drawLine(320, 0, 320, getHeight());
-        g2.setStroke(new BasicStroke(3));
-        g2.setColor(Color.GRAY);
-        g2.drawLine(405, 0, 405, getHeight());
-        g2.setStroke(new BasicStroke(2));
-        g2.drawLine(490, 0, 490, getHeight());
-        g2.setStroke(new BasicStroke(2));
-        g2.drawLine(575, 0, 575, getHeight());
-    }
+    g2.setColor(Color.orange);
+    g2.drawLine(150, 0, 150, getHeight());
+    g2.drawLine(235, 0, 235, getHeight());
+    g2.setStroke(new BasicStroke(4));
+    g2.drawLine(320, 0, 320, getHeight());
+    g2.setStroke(new BasicStroke(3));
+    g2.setColor(Color.GRAY);
+    g2.drawLine(405, 0, 405, getHeight());
+    g2.setStroke(new BasicStroke(2));
+    g2.drawLine(490, 0, 490, getHeight());
+    g2.setStroke(new BasicStroke(2));
+    g2.drawLine(575, 0, 575, getHeight());
+  }
 
 }
