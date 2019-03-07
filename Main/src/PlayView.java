103670011f18aea8f7c33cc3013b0f55aa074e93
git <vangerwen.willem@gmail.com>
2019-03-07 13:54:02 +0000

    LANEPANEL NOT JPANEL

--- Main/src/PlayView.java
+++ Main/src/PlayView.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.OverlayLayout;
 
 /**
  * @author Tom
@@ -20,7 +19,7 @@ public class PlayView extends JFrame implements PropertyChangeListener {
   private PlayModel model;
 
   private ViewMain.guitar guitar;
-  private JPanel jp1,jp2,jp3;
+  private LanePanel jp1,jp2,jp3;
 
   public PlayView(PlayModel model) {
     this.model = model;
@@ -48,17 +47,10 @@ public class PlayView extends JFrame implements PropertyChangeListener {
   }
 
   public void propertyChange(PropertyChangeEvent pce) {
-    this.guitar.remove(this.jp1);
-    this.guitar.remove(this.jp2);
-    this.guitar.remove(this.jp3);
+    this.jp1.setNotes((ArrayList<Note>)pce.getNewValue());
+    this.jp2.setNotes((ArrayList<Note>)pce.getNewValue());
+    this.jp3.setNotes((ArrayList<Note>)pce.getNewValue());
 
-    this.jp1 = new LanePanel((ArrayList<Note>)pce.getNewValue(),0);
-    this.jp2 = new LanePanel((ArrayList<Note>)pce.getNewValue(),1);
-    this.jp3 = new LanePanel((ArrayList<Note>)pce.getNewValue(),2);
-
-    this.guitar.add(jp1);
-    this.guitar.add(jp2);
-    this.guitar.add(jp3);
     this.revalidate();
     this.repaint();
     this.pack();
@@ -68,36 +60,6 @@ public class PlayView extends JFrame implements PropertyChangeListener {
     PlayModel mp = new PlayModel();
     PlayView vp = new PlayView(mp);
     PlayController1 cp = new PlayController1(mp);
-  }
-
-  public class LanePanel extends JPanel {
-    private ArrayList<Note> notes;
-    private BufferedImage white;
-    private BufferedImage black;
-    private int lane;
-    public LanePanel(ArrayList<Note> notes, int lane) {
-      try{
-        this.white = ImageIO.read(new FileInputStream("Main/src/resources/pick.png"));
-        this.black = ImageIO.read(new FileInputStream("Main/src/resources/pick1.png"));
-      }catch(IOException e) {
-        System.out.println(e.getMessage());
-        System.exit(1);
-      }
-      this.lane = lane;
-      this.notes = notes;
-      this.setOpaque(false);
-    }
-    public void paintComponent(Graphics g) {
-      for(Note n : this.notes) {
-        if(n.getLane() == lane) {
-          if(n.getColour() == 1) {
-            g.drawImage(white, 100, n.getY(), null);
-          } else {
-            g.drawImage(black, 100, n.getY(), null);
-          }
-        }
-      }
-    }
   }
 
 }
