79af9c9dbdf95a5ca2f1a7e94e63ab60f8748656
lhs211 <lhs211@exeter.ac.uk>
2019-03-07 15:47:04 +0000

    RIDING SOLO

--- Main/src/ExtractNotes.java
+++ Main/src/ExtractNotes.java
@@ -38,22 +38,19 @@ public class ExtractNotes{
    * @param args
    */
   public static void main(String[] args){
-    /*
     try{
       seq = MidiSystem.getSequence(new File( FILE));
     } catch (Exception e){
       e.printStackTrace();
       System.exit(1);
     }
-    */
 
-    playBackingTrack("AllTheSmallThings");
-    /*
+    //playBackingTrack("AllTheSmallThings");
+
     Guitar leadGuitar = findLeadGuitar(findAllGuitars(seq));
     guitarTrack = leadGuitar.getTrackNumber();
-    convertMidiToNotes(seq, leadGuitar.getInstrumentNumber());
-    playWithoutGuitar(leadGuitar);
-    */
+    //convertMidiToNotes(seq, leadGuitar.getInstrumentNumber());
+    playGuitar(leadGuitar);
   }
 
   /**
@@ -342,7 +339,40 @@ public class ExtractNotes{
     }
   }
 
-  public static void playWithoutGuitar(Guitar leadGuitar){
+  public static void playSoloGuitar(String filename){
+    try{
+      seq = MidiSystem.getSequence(new File( "Main/src/" + filename + ".mid"));
+    } catch (Exception e){
+      e.printStackTrace();
+      System.exit(1);
+    }
+    Guitar leadGuitar = findLeadGuitar(findAllGuitars(seq));
+    guitarTrack = leadGuitar.getTrackNumber();
+
+    try {
+      final Sequencer sequen = MidiSystem.getSequencer();
+      final Transmitter trans  = sequen.getTransmitter();
+
+      sequen.open();
+
+      sequen.setSequence( MidiSystem.getSequence( new File( "Main/src/" + filename + ".mid" ) ) );
+      sequen.setTrackSolo(leadGuitar.getTrackNumber(), true);
+
+      sequen.addMetaEventListener( new MetaEventListener() {
+        public void meta( MetaMessage mesg ) {
+          if ( mesg.getType() == 0x2F /* end-of-track */ ) {
+            sequen.close();
+          }
+        }
+      });
+      sequen.start();
+    } catch ( Exception exn ) {
+      System.out.println( exn );
+      System.exit( 1 );
+    }
+  }
+
+  public static void playGuitar(Guitar leadGuitar){
     try {
       final Sequencer sequen = MidiSystem.getSequencer();
       final Transmitter trans  = sequen.getTransmitter();
