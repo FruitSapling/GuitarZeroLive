f3ac92cc3307b9fac3de5e2bbe909a27ffc6f8f0
git <vangerwen.willem@gmail.com>
2019-03-18 16:27:29 +0000

    idk

--- Main/src/MainModel.java
+++ Main/src/MainModel.java
@@ -48,13 +48,6 @@ public class MainModel {
     this.support.firePropertyChange(null,null,null);
   }
   public void strummed() {
-<<<<<<< HEAD
-  }
-=======
-    System.out.println("Strum Me Daddy");
-  } //TODO: Take this out on friday ;)
->>>>>>> 792ea64ecd1e917a07815a81ea5cb36a485443bd
-  public void down() {
   }
 }
 
