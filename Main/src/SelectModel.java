3e8dfdaf88c36794af713b3a4c8f8ac25b5dd48e
MorganC46 <morgancentini38@gmail.com>
2019-03-19 12:04:23 +0000

    Little bit of MVC

--- Main/src/SelectModel.java
+++ Main/src/SelectModel.java
@@ -3,8 +3,16 @@
  * Refactored for Select Mode from Slash Mode by @Morgan
  */
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SelectModel {
     public boolean menuOpen;
@@ -45,4 +53,121 @@ public class SelectModel {
     public void hideMenu() {
         this.support.firePropertyChange(null,null,null);
     }
+
+    CarouselButton[] setMenu(JFrame frame) {
+        /**
+         * @author Morgan
+         */
+        File folder = new File(Constants.ZIP_FILE_PATH + "/");
+        File[] list = folder.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) { return file.isDirectory(); }
+        });
+
+        CarouselButton[] buttons = new CarouselButton[5];
+        ArrayList<File> arrayList = new ArrayList<File>(Arrays.asList(list));
+
+        if (list.length < 5) {
+            int surplus = 5 - (5-list.length);
+
+            for (int i = 0; i < list.length; i++) {
+                buttons[i] = createMeaningfulButton(frame, arrayList, i);
+            }
+            for (int i = surplus; i < 5; i++) {
+                buttons[i] = createEmptyButton();
+            }
+        }
+
+        else if (list.length > 5) {
+            //TODO: Handle Carousel Menu with more than 5 buttons
+        }
+
+        else {
+            for (int i = 0; i < list.length; i++) {
+                buttons[i] = createMeaningfulButton(frame, arrayList, i);
+            }
+        }
+        return buttons;
+    }
+
+    CarouselButton createMeaningfulButton(JFrame frame, ArrayList<File> list, int count) {
+        /**
+         * @author Morgan
+         */
+        String zipName = list.get(count).getName();
+        String zipPath = Constants.ZIP_FILE_PATH + "/" + list.get(count).getName() + zipName + "/" + ".midnotes";
+
+        File folder = new File(Constants.ZIP_FILE_PATH + "/" + zipName + "/");
+        ArrayList<File> unzippedList = new ArrayList<>(Arrays.asList(folder.listFiles()));
+        String imagePath = Constants.DEFAULT_WHITE_IMAGE_PATH;
+
+        for (int j = 0; j < unzippedList.size(); j++) {
+            if (unzippedList.get(j).getName().split("\\.")[1].equals("jpg")) {
+                imagePath = unzippedList.get(j).getPath();
+            }
+        }
+        File img = new File(imagePath);
+
+        try {
+            Image image = ImageIO.read(img);
+            Image newImage = image.getScaledInstance(Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT, Image.SCALE_DEFAULT);
+            ImageIcon icon = new ImageIcon(newImage);
+
+            return new CarouselButton(icon, zipName) { @Override public void onClick() {
+                IntendedTrack.setIntendedTrack(zipPath.replace("\\", "/"));
+                JOptionPane.showMessageDialog(null, "Selected track has become: " + zipName,
+                        "Selection Info", JOptionPane.INFORMATION_MESSAGE);
+
+                frame.dispose();
+                MainModel model = new MainModel();
+                MainController controller1 = new MainController(model);
+                MainGuitarController controller2 = new MainGuitarController(model);
+                new MainView(model, controller1, controller2);
+            }
+            };
+        }
+        catch (IOException e) {
+            System.exit(2);
+        }
+        return null;
+    }
+
+    CarouselButton createEmptyButton() {
+        /**
+         * @author Morgan
+         */
+        String imagePath = Constants.DEFAULT_WHITE_IMAGE_PATH;
+        File img = new File(imagePath);
+
+        try {
+            Image image = ImageIO.read(img);
+            Image newImage = image.getScaledInstance(Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT, Image.SCALE_DEFAULT);
+            ImageIcon icon = new ImageIcon(newImage);
+
+            return new CarouselButton(icon, "empty") {
+                @Override
+                public void onClick() {
+                    JOptionPane.showMessageDialog(null, "Nothing to see here...",
+                            "Empty Button", JOptionPane.WARNING_MESSAGE);
+                }
+            };
+        }
+        catch (IOException e) {
+            System.exit(2);
+        }
+        return null;
+    }
+
+    //    private ArrayList<File> inputAllFiles() {
+//        File folder = new File(Constants.ZIP_FILE_PATH + "/");
+//
+//        //TODO: Figure out why this validation check isnt working
+////        for (int i = 0; i < list.size(); i++) {
+////            int index = list.get(i).getName().lastIndexOf('.');
+////            if (list.get(i).getName().substring(index) != "zip") {
+////                list.remove(i);
+////            }
+////        }
+//        return new ArrayList<File>(Arrays.asList(folder.listFiles()));
+//    }
 }
--- Main/src/SelectView.java
+++ Main/src/SelectView.java
@@ -38,7 +38,7 @@ public class SelectView extends JFrame implements PropertyChangeListener {
         this.panel = new JPanel();
         this.panel.setPreferredSize(new Dimension(Constants.w,Constants.h));
 
-        CarouselButton[] buttons = setMenu(this);
+        CarouselButton[] buttons = model.setMenu(this);
         this.menu = new CarouselMenu(buttons, 20, 400);
         this.model.addPropertyChangeListener(menu);
 
@@ -65,119 +65,6 @@ public class SelectView extends JFrame implements PropertyChangeListener {
         this.revalidate();
         this.repaint();
         this.pack();
-    }
-
-
-    private CarouselButton[] setMenu(JFrame frame) {
-        /**
-         * @author Morgan
-         */
-        File folder = new File(Constants.ZIP_FILE_PATH + "/");
-        File[] list = folder.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File file) { return file.isDirectory(); }
-        });
-
-        CarouselButton[] buttons = new CarouselButton[5];
-        ArrayList<File> arrayList = new ArrayList<File>(Arrays.asList(list));
-
-        if (list.length < 5) {
-            int surplus = 5 - (5-list.length);
-
-            for (int i = 0; i < list.length; i++) {
-                buttons[i] = createMeaningfulButton(frame, arrayList, i);
-            }
-            for (int i = surplus; i < 5; i++) {
-                buttons[i] = createEmptyButton();
-            }
-        }
-
-        else if (list.length > 5) {
-            //TODO: Handle Carousel Menu with more than 5 buttons
-        }
-
-        else {
-            for (int i = 0; i < list.length; i++) {
-                buttons[i] = createMeaningfulButton(frame, arrayList, i);
-            }
-        }
-        return buttons;
-    }
-
-    private CarouselButton createMeaningfulButton(JFrame frame, ArrayList<File> list, int count) {
-
-        String zipName = list.get(count).getName();
-        String zipPath = Constants.ZIP_FILE_PATH + "/" + list.get(count).getName() + zipName + "/" + ".midnotes";
-
-        File folder = new File(Constants.ZIP_FILE_PATH + "/" + zipName + "/");
-        ArrayList<File> unzippedList = new ArrayList<>(Arrays.asList(folder.listFiles()));
-        String imagePath = Constants.DEFAULT_WHITE_IMAGE_PATH;
-
-        for (int j = 0; j < unzippedList.size(); j++) {
-            if (unzippedList.get(j).getName().split("\\.")[1].equals("jpg")) {
-                imagePath = unzippedList.get(j).getPath();
-            }
-        }
-        File img = new File(imagePath);
-
-        try {
-            Image image = ImageIO.read(img);
-            Image newImage = image.getScaledInstance(Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT, Image.SCALE_DEFAULT);
-            ImageIcon icon = new ImageIcon(newImage);
-
-            return new CarouselButton(icon, zipName) { @Override public void onClick() {
-                    IntendedTrack.setIntendedTrack(zipPath.replace("\\", "/"));
-                    JOptionPane.showMessageDialog(null, "Selected track has become: " + zipName,
-                            "Selection Info", JOptionPane.INFORMATION_MESSAGE);
-
-                    frame.dispose();
-                    MainModel model = new MainModel();
-                    MainController controller1 = new MainController(model);
-                    MainGuitarController controller2 = new MainGuitarController(model);
-                    new MainView(model, controller1, controller2);
-                }
-            };
-        }
-        catch (IOException e) {
-            System.exit(2);
-        }
-        return null;
-    }
-
-    private CarouselButton createEmptyButton() {
-        String imagePath = Constants.DEFAULT_WHITE_IMAGE_PATH;
-        File img = new File(imagePath);
-
-        try {
-            Image image = ImageIO.read(img);
-            Image newImage = image.getScaledInstance(Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT, Image.SCALE_DEFAULT);
-            ImageIcon icon = new ImageIcon(newImage);
-
-            return new CarouselButton(icon, "empty") {
-                @Override
-                public void onClick() {
-                    JOptionPane.showMessageDialog(null, "Nothing to see here...",
-                            "Empty Button", JOptionPane.WARNING_MESSAGE);
-                }
-            };
-        }
-        catch (IOException e) {
-            System.exit(2);
-        }
-        return null;
-    }
-
-    private ArrayList<File> inputAllFiles() {
-        File folder = new File(Constants.ZIP_FILE_PATH + "/");
-
-        //TODO: Figure out why this validation check isnt working
-//        for (int i = 0; i < list.size(); i++) {
-//            int index = list.get(i).getName().lastIndexOf('.');
-//            if (list.get(i).getName().substring(index) != "zip") {
-//                list.remove(i);
-//            }
-//        }
-        return new ArrayList<File>(Arrays.asList(folder.listFiles()));
     }
 
 }
