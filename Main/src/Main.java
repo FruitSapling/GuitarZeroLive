893c6286191ad68a29527e19d8fd81b85dc4980a
MarkcNewell <markcnewell350@gmail.com>
2019-03-21 19:21:53 +0000

    fucking fixed it

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,5 +4,14 @@ public class Main {
     MainGuitarController controller1 = new MainGuitarController(model);
     MainController controller2 = new MainController(model);
     MainView mainMenu = new MainView(model, controller2, controller1);
+
+
+    // Hot fix for carousel bug.
+    model.menuOpen = true;
+    model.showMenu();
+    model.showMenu();
+
+    mainMenu.requestFocusInWindow();
+
   }
 }
